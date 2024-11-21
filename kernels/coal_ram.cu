3a5b99e450793fe1b6dda6a4c8facebbb77ae1e9
JesseYan64 <primeamber@outlook.com>
2024-11-20 20:22:24 -0600

    minor

--- kernels/coal_ram.cu
+++ kernels/coal_ram.cu
@@ -5,7 +5,7 @@
 // Naive kernel implementation
 __global__ void coalRamMatrixMulKernel(float* A, float* B, float* C, int M, int N, int K, float alpha, float beta) {
     const int row = blockIdx.x * TILE_WIDTH + (threadIdx.x / TILE_WIDTH);
-    const int col = blockIdx.y * TILE_WIDTH + (threadIdx.y % TILE_WIDTH);
+    const int col = blockIdx.y * TILE_WIDTH + (threadIdx.x % TILE_WIDTH);
     
     if (row < M && col < N) {
         float sum = 0.0f;
@@ -19,7 +19,7 @@ __global__ void coalRamMatrixMulKernel(float* A, float* B, float* C, int M, int 
 // Naive wrapper function
 void coalRamMatrixMul(float* A, float* B, float* C, int M, int N, int K, float alpha, float beta) {
     dim3 gridDim(CEIL(M, TILE_WIDTH), CEIL(N, TILE_WIDTH));
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
+    dim3 blockDim(TILE_WIDTH * TILE_WIDTH);
     
     coalRamMatrixMulKernel<<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
 }
