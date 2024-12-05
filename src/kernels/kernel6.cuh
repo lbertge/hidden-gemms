41e0b47e5dd9264954c4f5005ed20dd6c2000273
JesseYan64 <primeamber@outlook.com>
2024-12-05 06:32:53 -0600

    vectorize

--- src/kernels/kernel6.cuh
+++ src/kernels/kernel6.cuh
@@ -5,7 +5,7 @@
 // Vectorize kernel
 template <const int BM, const int BN, const int BK, const int TM, const int TN>
 __global__ void vectorized_kernel(float *A, float *B, float *C, int M, int N, int K, float alpha, float beta) {
-    __shared__ float As[BM][BK];
+    __shared__ float As[BK][BM];
     __shared__ float Bs[BK][BN];
 
     int bx = blockIdx.x;
