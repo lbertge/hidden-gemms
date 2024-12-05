34f8c114c2afbf4a34b4b89f82234788ebd8656b
JesseYan64 <primeamber@outlook.com>
2024-12-05 03:12:44 -0600

    1d block_tiling

--- src/kernels/kernel4.cuh
+++ src/kernels/kernel4.cuh
@@ -3,6 +3,57 @@
 // 1D block tiling kernel
 template <const int BM, const int BN, const int BK, const int TM>
 __global__ void block_tiling_1d_kernel(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta) {
+    __shared__ float As[BM][BK];
+    __shared__ float Bs[BK][BN];
+
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int thread_num = blockDim.x;
+
+    int tx = threadIdx.x % BN;
+    int ty = threadIdx.x / BN * TM;
+
+    int AStart = by * BM * K;
+    int BStart = bx * BN;
+    int CStart = by * BM * N + bx * BN;
+
+    int AsRow = threadIdx.x / BK;
+    int AsCol = threadIdx.x % BK;
+
+    int BsRow = threadIdx.x / BN;
+    int BsCol = threadIdx.x % BN;
+
+    float reg[TM + 1] = {0.0};
+
+    #pragma unroll
+    for (int k = 0; k < K; k += BK) {
+      As[AsRow + i][AsCol] = A[AStart + (AsRow + i) * K + AsCol];
+      Bs[BsRow + i][BsCol] = B[BStart + (BsRow + i) * N + BsCol];
+      __syncthreads();
+
+      AStart += BK;
+      BStart += BK * N;
+
+      #pragma unroll
+      for (int i = 0; i < BK; ++i) {
+        reg[TM] = Bs[i][tx];
+        #pragma unroll
+        for (int j = 0; j < TM; ++j) {
+          reg[j] += As[ty + j][i] * reg[TM];
+        }
+      }
+      __syncthreads();
+    }
+
+    #pragma unroll
+    for (int i = 0; i < TM; ++i) {
+      C[CStart + (ty + i) * N + tx] = alpha * reg[i] + beta * C[CStart + (ty + i) * N + tx];
+    }
+}
+
+// 1D block tiling kernel
+template <const int BM, const int BN, const int BK, const int TM>
+__global__ void block_tiling_1d_kernel_bk(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta) {
   // If we flip x and y here we get ~30% less performance for large matrices.
   // The current, 30% faster configuration ensures that blocks with sequential
   // blockIDs access columns of B sequentially, while sharing the same row of A.
@@ -66,62 +117,4 @@ __global__ void block_tiling_1d_kernel(const float *A, const float *B, float *C,
         alpha * threadResults[resIdx] +
         beta * C[(threadRow * TM + resIdx) * N + threadCol];
   }
-}
-
-template <const int BM, const int BN, const int BK, const int TM>
-__global__ void block_tiling_1d_kernel_bk(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta) {
-    __shared__ float As[BM][BK];
-    __shared__ float Bs[BK][BN];
-
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-    int thread_num = blockDim.x;
-
-    int tx = threadIdx.x % BN;
-    int ty = threadIdx.x / BN * TM;
-
-    int AStart = by * BM * K;
-    int BStart = bx * BN;
-    int CStart = by * BM * N + bx * BN;
-
-    int AsRow = threadIdx.x / BK;
-    int AsCol = threadIdx.x % BK;
-    int AsStep = thread_num / BK;
-
-    int BsRow = threadIdx.x / BN;
-    int BsCol = threadIdx.x % BN;
-    int BsStep = thread_num / BN;
-
-    float reg[TM + 1] = {0.0};
-
-    #pragma unroll
-    for (int k = 0; k < K; k += BK) {
-      #pragma unroll
-      for (int i = 0; i < BM; i += AsStep) {
-        As[AsRow + i][AsCol] = A[AStart + (AsRow + i) * K + AsCol];
-      }
-      #pragma unroll
-      for (int i = 0; i < BK; i += BsStep) {
-        Bs[BsRow + i][BsCol] = B[BStart + (BsRow + i) * N + BsCol];
-      }
-      __syncthreads();
-
-      AStart += BK;
-      BStart += BK * N;
-
-      #pragma unroll
-      for (int i = 0; i < BK; ++i) {
-        reg[TM] = Bs[i][tx];
-        #pragma unroll
-        for (int j = 0; j < TM; ++j) {
-          reg[j] += As[ty + j][i] * reg[TM];
-        }
-      }
-      __syncthreads();
-    }
-
-    #pragma unroll
-    for (int i = 0; i < TM; ++i) {
-      C[CStart + (ty + i) * N + tx] = alpha * reg[i] + beta * C[CStart + (ty + i) * N + tx];
-    }
 }
